5f6bffce39db5e470c2e8af9d2fc49fb08c3b782
Smartisek <domalipdominik@gmail.com>
2023-10-06 12:12:37 +0100

    testing

--- src/JunitMethods.java
+++ src/JunitMethods.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class JunitMethods {
+
+
+public class JunitMethods {
+
+    public boolean multiple(int num1){
+        if(num1 %3 ==0 || num1%5 ==0){
+            return true;
+        } else
+            return false;
+    }
+
+    public int checkLargest(int num1, int num2, int num3){
+        int firstMax = Math.max(num1, num2);
+
+        return Math.max(firstMax, num3);
+    }
+
+
+
 }
